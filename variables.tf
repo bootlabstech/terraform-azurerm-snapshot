2425aad3e7694ea2b17da1fd80f2ca05362097a2
lalitha2407 <lalitha@bootlabstech.com>
2023-04-04 17:43:39 +0530

    initial

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "name" {
+  description = "value"
+  type        = string
+
+}
+variable "location" {
+  description = "value"
+  type        = string
+
+}
+variable "resource_group_name" {
+  description = "value"
+  type        = string
+
+}
+variable "create_option" {
+  description = "value"
+  type        = string
+
+}
+variable "source_uri" {
+  description = "value"
+  type        = string
+
+}
+variable "source_resource_id" {
+  description = "value"
+  type        = string
+
+}
+variable "disk_size_gb" {
+  description = "value"
+  type        = string
+
+}
