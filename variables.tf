f33d9eeb0ff7caaa41ac59f59eeb6b5a44d78c83
lalita2407 <lalitalallu02@gmail.com>
2023-09-18 13:47:10 +0530

    changed provider version 3.22 to 3.64

--- variables.tf
+++ variables.tf
@@ -2,31 +2,25 @@
 variable "name" {
   description = "Specifies the name of the Snapshot resource."
   type        = string
-
 }
 variable "location" {
   description = "Specifies the supported Azure location where the resource exists."
   type        = string
-
 }
 variable "resource_group_name" {
   description = "The name of the resource group in which to create the Snapshot"
   type        = string
-
 }
 variable "create_option" {
   description = "ndicates how the snapshot is to be created. Possible values are Copy or Import."
   type        = string
   default = "Copy"
-
 }
 variable "source_uri" {
   description = "value"
   type        = string
-
 }
 variable "disk_size_gb" {
   description = "The size of the Snapshotted Disk in GB."
   type        = number
-
 }
